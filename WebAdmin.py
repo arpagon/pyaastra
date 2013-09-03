0882988ef1ef192371e5342edd0de60be6569453
Sebastian Rojo <arpagon@gmail.com>
2013-09-03 17:33:04 -0400

    Fix Virtual Envs

--- WebAdmin.py
+++ WebAdmin.py
@@ -30,6 +30,9 @@ __author__ = "Sebastian Rojo <http://www.sapian.com.co> arpagon@gamil.com"
 __contributors__ = []
 _debug = 0
 
+activate_this = "/opt/DialBox/bin/activate_this.py"
+execfile(activate_this, dict(__file__=activate_this))
+
 from optparse import OptionParser
 from mechanize import Browser
 import subprocess
--- nmap.py
+++ nmap.py
@@ -28,6 +28,10 @@ __author__ = "Sebastian Rojo <http://www.sapian.com.co> arpagon@gamil.com"
 __contributors__ = []
 _debug = 0
 
+
+activate_this = "/opt/DialBox/bin/activate_this.py"
+execfile(activate_this, dict(__file__=activate_this))
+
 import subprocess
 from BeautifulSoup import BeautifulSoup
 import logging
